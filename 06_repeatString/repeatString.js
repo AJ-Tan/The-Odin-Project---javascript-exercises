fad863a6ed2ce766195c5202bc88e75407f3ced0
AJ-Tan <aldomin22@gmail.com>
2025-11-17 21:18:05 +0800

    Solved 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+  let output = num >= 0 ? '' : 'ERROR';
+  while(num > 0) {
+    output += str;
+    num--;
+  }
+  return output;
 };
 
 // Do not edit below this line
