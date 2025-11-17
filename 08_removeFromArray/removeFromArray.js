d4fe48db932b7ef3c74e07bf9683699dc66cc361
AJ-Tan <aldomin22@gmail.com>
2025-11-17 21:40:19 +0800

    Solved: 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...filterValue) {
+  return arr.filter(val => !filterValue.includes(val));
 };
 
 // Do not edit below this line
