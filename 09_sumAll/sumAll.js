238942ca33b324f1f5a897ec39c737e5d707c807
AJ-Tan <aldomin22@gmail.com>
2025-11-17 22:21:54 +0800

    Solved: 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+  const isInvalid = val => {
+    if(!Number.isInteger(val)) return true;
+    if(val < 0) return true;
+    return false;
+  }
 
+  if(isInvalid(min) || isInvalid(max)) return "ERROR";
+
+  if(min > max) [min, max] = [max, min];
+
+  let sum = 0;
+  
+  for(min; min <= max; min++) {
+    sum+=min;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
