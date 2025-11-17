72ecb5d9bd2ff47ecc6c051aa6248f5b58194038
AJ-Tan <aldomin22@gmail.com>
2025-11-17 22:34:59 +0800

    Solved: 10_leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(paramYear) {
+  if(paramYear % 4 === 0 && paramYear % 100 !== 0 
+    || paramYear % 400 === 0) {
+    return true;
+  } else {
+    return false
+  }
 };
 
 // Do not edit below this line
