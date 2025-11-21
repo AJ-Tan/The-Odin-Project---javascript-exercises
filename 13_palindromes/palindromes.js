ef3f43d8756ac45b95a9aa3feedd2eaf3e42ff8b
AJ-Tan <aldomin22@gmail.com>
2025-11-21 18:41:24 +0800

    Solved: 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const validCharacter = 'abcdefghijklmnopqrstuvwxyz123456789';
 
+  const cleanString = [...str.toLowerCase()]
+  .filter(val => validCharacter.includes(val))
+  .join('');
+  return cleanString === [...cleanString].reverse().join('');
 };
 
 // Do not edit below this line
