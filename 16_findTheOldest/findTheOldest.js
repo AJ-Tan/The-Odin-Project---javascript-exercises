83dfa43f096510adc169118f684db6d09a058e51
AJ-Tan <aldomin22@gmail.com>
2025-11-21 19:13:58 +0800

    Solved: 16_findtheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  const getAge = ({yearOfBirth, yearOfDeath}) => {  
+    const deathYear = yearOfDeath ? yearOfDeath : new Date().getFullYear();
+    return deathYear - yearOfBirth;
+  }
 
+  return arr.sort((a,b) => {
+    const ageA = getAge(a), ageB = getAge(b);
+    return ageB - ageA;
+  })[0];
 };
 
 // Do not edit below this line
