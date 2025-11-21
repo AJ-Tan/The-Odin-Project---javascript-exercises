7939b47377de7150fca9976f94a681709ca65233
AJ-Tan <aldomin22@gmail.com>
2025-11-21 18:56:47 +0800

    Solved: 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(fibNumber) {
+  if(+fibNumber < 0) return 'OOPS';
+  if(+fibNumber === 0) return 0;
+  if(+fibNumber <= 2) return 1;
 
+  let [prevValue, currentValue] = [1, 1];
+  for(let i = 2; i < fibNumber; i++) {
+    [prevValue, currentValue] = [currentValue, prevValue + currentValue];
+  }
+  return currentValue;
 };
 
 // Do not edit below this line
